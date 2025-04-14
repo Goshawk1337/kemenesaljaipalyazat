320c7300d097a85739984cbe9d15aeeaae6cf093
Goshawk1337 <77876216+Goshawk1337@users.noreply.github.com>
2025-04-14 12:04:46 +0200

    Footer fix, tobb ember

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,4 +1,3 @@
-import { div } from 'motion/react-client';
 import React from 'react';
 
 const Footer: React.FC = () => {
@@ -6,7 +5,7 @@ const Footer: React.FC = () => {
     <div className='w-[64.583vw] mx-auto '>
       <footer className="bg-neutral-900 text-white py-4 mx-auto">
         <div className="max-w-screen-xl mt-16 space-x-4">
-          <p className="">Készítette: <a href="https://goshawk.hu" target='_blank' className="text-neutral-400 hover:text-white">Biri Zsombor</a></p>
+          <p className="">Készítette: <a href="https://goshawk.hu" target='_blank' className="text-neutral-400 hover:text-white">Gosk</a></p>
         </div>
       </footer>
     </div>
