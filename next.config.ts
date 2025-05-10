df7b86f03749a4774e09a4755557d13ee8c0033c
Goshawk1337 <fivemscipthun@gmail.com>
2025-05-10 09:35:03 +0200

    another building issue fix

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,8 @@ const nextConfig: NextConfig = {
   /* config options here */
     output: 'export',
     distDir: 'build',
+    trailingSlash: true,
+
     images: {
       unoptimized: true,
     },
