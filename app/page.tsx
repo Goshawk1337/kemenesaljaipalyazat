c10892745215d4c36138148b2a7aaa1c2961cc4f
Goshawk1337 <fivemscipthun@gmail.com>
2025-05-13 14:54:33 +0200

    spelling fix

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
           <div className="text-neutral-100 text-center   ">
             <h1 className="font-bold md:text-[2.5rem]  text-2xl">Üdv a honlapon!</h1>
             <div className="text-center">
-              <p className="font-thin md:text-[1.25rem] text-xs max-w-[400px]  text-neutral-100  mt-2 mx-auto text-justify">A honlapot olyan céllel készítettem, hogy bemutassam Kemenesalja nevezetes helyeit, és híres személyeit, mind ezt egy jól kinéző letisztult dizájn-nal megáldva. </p>
+              <p className="font-thin md:text-[1.25rem] text-xs max-w-[400px]  text-neutral-100  mt-2 mx-auto text-justify">A honlapot azzal a céllal készítettem, hogy bemutassam Kemenesalja nevezetes helyeit, és híres személyeit, mind ezt egy jól kinéző letisztult design-nal megáldva. </p>
               <div className="flex-row ">
                 <Link href={"/kornyek"}><Button variant={"ghost"} className="w-[10rem] mt-5 cursor-pointer transition-all duration-500 hover:bg-[#27285C] ">A környék</Button></Link>
                 <Link href={"/hiresek"}><Button variant={"ghost"} className="w-[10rem] mt-5 cursor-pointer transition-all duration-500 hover:bg-[#27285C] ml-3  ">A Híres személyek</Button></Link>
